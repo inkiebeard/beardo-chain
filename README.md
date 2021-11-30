b70b05e8f8b661966dd37b5c2f8e05ac457f7d53
Aaron Toomey <toomey.aaron@gmail.com>
2021-11-30 22:54:41 +1100

    Remove lib pollutants + JSON to & from

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # Beardo Chain
 
+## Purpose
+
+To provide a backbone for determining data capital goods and ensure data progenitor and lineage. THIS IS NOT A DECENTRALISED CRYPTO COIN. Firstly branching is encouraged, there are core functions to find the count 
+and place within the lineage. Value is ascribed to a block based on the "distance" from the initial source block of the chain. For example a blog post would be the initial block attached to the source of the chain, then each comment on that post would be attached **at to that same block as their parent** then a thread of replies would branch off one of those comments; the initial blog post as the starting block of the initial chain
+becomes greater in "value" the more ancestors it has.
+
 ### Usage
 
-Run the sandbox.html from the root directory - Proof of Concetop only
+Run the sandbox.html from the root directory - Proof of Concept only
 
 `// todo make readme`
-
-### Other Todos:
-* `setup vue app with proper stuff`
-* `setup backend`
-* `cdk for deploy of aws resources`
--- beardo-chain.js
+++ beardo-chain.js
@@ -3,7 +3,7 @@
  * @author Aaron Toomey <aaron@inkiebeard.codes>
  * @requires uuidv4
  * @requires sha256
- * todo: to & from json, localstorage save on each addition of block, calculate
+ * todo: verify chain(s), calculate "value" factor for block, allow weighting and external operators for value calc
  */
 
 class Block {
@@ -17,7 +17,7 @@ class Block {
   }
 
   _generateHash() {
-    const { _hash, ...hashable} = this
+    const { _hash, ...hashable} = this.map
     this._hash = sha256(JSON.stringify(hashable))
   }
 
@@ -39,6 +39,47 @@ class Block {
 
   get parentId() {
     return this._parentId
+  }
+
+  get map () {
+    return {
+      id: this._id,
+      data: this._data,
+      timestamp: this._timestamp,
+      parentId: this._parentId,
+      parentHash: this._parentHash,
+      hash: this._hash
+    }
+  }
+
+  toJSON() {
+    return JSON.stringify(this.map)
+  }
+
+  static fromJSON(json) {
+    try {
+      const tmp = JSON.parse(json)
+      const {
+        id,
+        data,
+        timestamp,
+        parentId,
+        parentHash,
+        hash
+      } = tmp
+      // yuck not sure how to do this better ಠ_ಠ
+      const block = new Block('to override', {id:'foo', hash:'bar'})
+      block._id = id
+      block._data = data
+      block._timestamp = timestamp
+      block._parentId = parentId
+      block._parentHash = parentHash
+      block._hash = hash
+      return block
+    } catch (e) {
+      console.error(e.message)
+      throw new Error('Invalid json string')
+    }
   }
 }
 
@@ -98,5 +139,28 @@ class Ledger {
     } while (child !== null)
 
     return { branch: [...ancestors, block, ...descendents], ancestors: ancestors.length, descendents: descendents.length }
+  }
+
+  toJSON () {
+    let blockMaps = Object.values(this.blocks).map(block => block.map)
+    return JSON.stringify(blockMaps)
+  }
+
+  static fromJSON(json) {
+    try {
+      const tmp = JSON.parse(json)
+      if (!Array.isArray(tmp)) {
+        throw new Error('Invalid array of blocks')
+      }
+      const ledger = new BeardoLedger()
+      tmp.forEach(item => {
+        ledger.blocks[item.id] = item
+      })
+      ledger.fixListChrono()
+      return ledger
+    } catch (e) {
+      console.error(e.message)
+      throw new Error(`Invalid json string: ${e.message}`)
+    }
   }
 }
